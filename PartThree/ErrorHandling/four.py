0a0e743787a075b1d90dffcf590fa09c1a72f8b1
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-03-14 08:30:38 +0530

    okie

--- PartThree/ErrorHandling/four.py
+++ PartThree/ErrorHandling/four.py
@@ -1,9 +1,8 @@
 # how to create custom/user defined errors
+
 class InsuffientFundsError(Exception):
     def __init__(self, arg):
         self.msg = arg
-
-
 amount = int(input("Enter Amount::::"))
 
 if amount > 5000:
